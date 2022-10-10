7075240d04c6e5fb4440ebb5e334ed648091905e
ericbrandt91 <ericbrandt91@gmail.com>
2022-10-10 20:50:43 +0200

    sqs example

---
+++ springwolf-examples/springwolf-sqs-example/src/main/java/io/github/stavshamir/springwolf/example/configuration/AsyncApiConfiguration.java
@@ -0,0 +1,65 @@
+package io.github.stavshamir.springwolf.example.configuration;
+
+import com.asyncapi.v2.model.info.Info;
+import com.asyncapi.v2.model.server.Server;
+import io.github.stavshamir.springwolf.asyncapi.types.AmqpConsumerData;
+import io.github.stavshamir.springwolf.asyncapi.types.AmqpProducerData;
+import io.github.stavshamir.springwolf.configuration.AsyncApiDocket;
+import io.github.stavshamir.springwolf.configuration.EnableAsyncApi;
+import io.github.stavshamir.springwolf.example.dtos.AnotherPayloadDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAsyncApi
+public class AsyncApiConfiguration {
+
+    private final String amqpHost;
+    private final String amqpPort;
+
+    public AsyncApiConfiguration(
+            @Value("${spring.rabbitmq.host}") String amqpHost,
+            @Value("${spring.rabbitmq.port}") int amqpPort) {
+        this.amqpHost = amqpHost;
+        this.amqpPort = String.valueOf(amqpPort);
+    }
+
+    @Bean
+    public AsyncApiDocket asyncApiDocket() {
+        Info info = Info.builder()
+                .version("1.0.0")
+                .title("Springwolf example project - AMQP")
+                .build();
+
+        Server amqp = Server.builder()
+                .protocol("amqp")
+                .url(String.format("%s:%s", amqpHost, amqpPort))
+                .build();
+
+        AmqpProducerData exampleProducer = AmqpProducerData.amqpProducerDataBuilder()
+                .queueName("example-producer-channel")
+                .description("example-producer-channel-description")
+                .exchangeName("example-topic-exchange")
+                .routingKey("example-topic-routing-key")
+                .payloadType(AnotherPayloadDto.class)
+                .build();
+
+        AmqpConsumerData exampleManuallyDefinedConsumer = AmqpConsumerData.amqpConsumerDataBuilder()
+                .queueName("example-manual-consumer-channel")
+                .description("example-manual-consumer-channel-description")
+                .exchangeName("example-consumer-topic-exchange")
+                .routingKey("example-consumer-topic-routing-key")
+                .payloadType(AnotherPayloadDto.class)
+                .build();
+
+        return AsyncApiDocket.builder()
+                .basePackage("io.github.stavshamir.springwolf.example.consumers")
+                .info(info)
+                .server("amqp", amqp)
+                .producer(exampleProducer)
+                .consumer(exampleManuallyDefinedConsumer)
+                .build();
+    }
+
+}
---
+++ springwolf-examples/springwolf-sqs-example/src/main/java/io/github/stavshamir/springwolf/example/consumers/ExampleConsumer.java
@@ -0,0 +1,32 @@
+package io.github.stavshamir.springwolf.example.consumers;
+
+import io.github.stavshamir.springwolf.example.dtos.AnotherPayloadDto;
+import io.github.stavshamir.springwolf.example.dtos.ExamplePayloadDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExampleConsumer.class);
+
+    @SqsListener("example-queue")
+    public void receiveExamplePayload(ExamplePayloadDto payload) {
+        logger.info("Received new message in example-queue: {}", payload.toString());
+    }
+
+    @SqsListener("another-queue")
+    public void receiveAnotherPayload(AnotherPayloadDto payload) {
+        logger.info("Received new message in another-queue: {}", payload.toString());
+    }
+
+
+
+}
